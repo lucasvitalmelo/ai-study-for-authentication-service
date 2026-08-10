acd61f67ae21d639b8a9f7b0f4ee6f9413ee5443
Lucas Vital <lucasvitaldemelo@gmail.com>
2026-08-10 13:12:24 -0300

    refactor: RegisterUserRequest.password usa @ValidPassword (issue #14)

--- src/main/java/dev/lucasvital/auth/user/RegisterUserRequest.java
+++ src/main/java/dev/lucasvital/auth/user/RegisterUserRequest.java
@@ -1,8 +1,8 @@
 package dev.lucasvital.auth.user;
 
 import dev.lucasvital.auth.validation.ValidEmail;
-import jakarta.validation.constraints.NotBlank;
+import dev.lucasvital.auth.validation.ValidPassword;
 import jakarta.validation.constraints.Size;
 
 public record RegisterUserRequest(
-        @ValidEmail @Size(max = 255) String email, @NotBlank String password) {}
+        @ValidEmail @Size(max = 255) String email, @ValidPassword String password) {}
