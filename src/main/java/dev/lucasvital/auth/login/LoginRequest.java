b32556e5966d1e38d733ec6fa5b26f42c0f0ccd5
Lucas Vital <lucasvitaldemelo@gmail.com>
2026-08-10 13:09:50 -0300

    refactor: LoginRequest.password usa @ValidPassword (issue #14)

--- src/main/java/dev/lucasvital/auth/login/LoginRequest.java
+++ src/main/java/dev/lucasvital/auth/login/LoginRequest.java
@@ -1,6 +1,6 @@
 package dev.lucasvital.auth.login;
 
 import dev.lucasvital.auth.validation.ValidEmail;
-import jakarta.validation.constraints.NotBlank;
+import dev.lucasvital.auth.validation.ValidPassword;
 
-public record LoginRequest(@ValidEmail String email, @NotBlank String password) {}
+public record LoginRequest(@ValidEmail String email, @ValidPassword String password) {}
